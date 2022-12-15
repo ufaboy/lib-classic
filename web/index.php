d8b43a68763d103b6f4326b960cc8c94e3ddfd04
Azat Kashapov <azat.developer@gmail.com>
2022-12-15 20:15:11 +0300

    test cd

--- web/index.php
+++ web/index.php
@@ -10,6 +10,7 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
+
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
