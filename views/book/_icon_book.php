947a58eaf8500a9158ffcfd329d10d0669c8ad27
Azat Kashapov <azat.developer@gmail.com>
2023-01-04 08:03:07 +0300

    Update _icon_book.php

--- views/book/_icon_book.php
+++ views/book/_icon_book.php
@@ -1,13 +1,20 @@
 <?php
 
 ?>
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="48px" height="48px" viewBox="0 0 24 24" version="1.1">
-    <g id="surface1">
-        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(8.627451%,62.745098%,52.156863%);fill-opacity:1;" d="M 3 8 L 3 21 C 3 22.105469 3.894531 23 5 23 L 19 23 C 20.105469 23 21 22.105469 21 21 L 21 8 Z M 3 8 "/>
-        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(92.54902%,94.117647%,94.509804%);fill-opacity:1;" d="M 3 7 L 3 20 C 3 21.101562 3.894531 22 5 22 L 19 22 C 20.105469 22 21 21.101562 21 20 L 21 7 Z M 3 7 "/>
-        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(74.117647%,76.470588%,78.039216%);fill-opacity:1;" d="M 3 6 L 3 19 C 3 20.101562 3.894531 21 5 21 L 19 21 C 20.105469 21 21 20.101562 21 19 L 21 6 Z M 3 6 "/>
-        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(92.54902%,94.117647%,94.509804%);fill-opacity:1;" d="M 3 5 L 3 18 C 3 19.101562 3.894531 20 5 20 L 19 20 C 20.105469 20 21 19.101562 21 18 L 21 5 Z M 3 5 "/>
-        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(8.627451%,62.745098%,52.156863%);fill-opacity:1;" d="M 5 1 C 3.894531 1 3 1.894531 3 3 L 3 21 C 3 22.105469 3.894531 23 5 23 L 7 23 L 7 22 L 5.5 22 C 4.671875 22 4 21.328125 4 20.5 C 4 19.671875 4.671875 19 5.5 19 L 19 19 C 20.105469 19 21 18.105469 21 17 L 21 3 C 21 1.894531 20.105469 1 19 1 Z M 5 1 "/>
-        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(10.196078%,73.72549%,61.176471%);fill-opacity:1;" d="M 8 1 L 8 19 L 19 19 C 20.105469 19 21 18.105469 21 17 L 21 3 C 21 1.894531 20.105469 1 19 1 Z M 8 1 "/>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="48px" height="48px"
+     viewBox="0 0 24 24" version="1.1">
+    <g>
+        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(8.627451%,62.745098%,52.156863%);fill-opacity:1;"
+              d="M 3 8 L 3 21 C 3 22.105469 3.894531 23 5 23 L 19 23 C 20.105469 23 21 22.105469 21 21 L 21 8 Z M 3 8 "/>
+        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(92.54902%,94.117647%,94.509804%);fill-opacity:1;"
+              d="M 3 7 L 3 20 C 3 21.101562 3.894531 22 5 22 L 19 22 C 20.105469 22 21 21.101562 21 20 L 21 7 Z M 3 7 "/>
+        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(74.117647%,76.470588%,78.039216%);fill-opacity:1;"
+              d="M 3 6 L 3 19 C 3 20.101562 3.894531 21 5 21 L 19 21 C 20.105469 21 21 20.101562 21 19 L 21 6 Z M 3 6 "/>
+        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(92.54902%,94.117647%,94.509804%);fill-opacity:1;"
+              d="M 3 5 L 3 18 C 3 19.101562 3.894531 20 5 20 L 19 20 C 20.105469 20 21 19.101562 21 18 L 21 5 Z M 3 5 "/>
+        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(8.627451%,62.745098%,52.156863%);fill-opacity:1;"
+              d="M 5 1 C 3.894531 1 3 1.894531 3 3 L 3 21 C 3 22.105469 3.894531 23 5 23 L 7 23 L 7 22 L 5.5 22 C 4.671875 22 4 21.328125 4 20.5 C 4 19.671875 4.671875 19 5.5 19 L 19 19 C 20.105469 19 21 18.105469 21 17 L 21 3 C 21 1.894531 20.105469 1 19 1 Z M 5 1 "/>
+        <path style=" stroke:none;fill-rule:nonzero;fill:rgb(10.196078%,73.72549%,61.176471%);fill-opacity:1;"
+              d="M 8 1 L 8 19 L 19 19 C 20.105469 19 21 18.105469 21 17 L 21 3 C 21 1.894531 20.105469 1 19 1 Z M 8 1 "/>
     </g>
 </svg>
