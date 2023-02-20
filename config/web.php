d359f991b15517b332bdbd2c7d8446aa0a3fa3ac
Azat Kashapov <azat.developer@gmail.com>
2023-02-20 09:39:46 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -28,12 +28,9 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer' => [
-            'class' => \yii\symfonymailer\Mailer::class,
-            'viewPath' => '@app/mail',
-            // send all mails to a file by default.
-            'useFileTransport' => true,
-        ],
+		'formatter' => [
+			'dateFormat' => 'dd.MM.yy',
+		],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
